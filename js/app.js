2a09e52e572ef11eda83ac67739446574766c94f
shagors <shajjadshagor@gmail.com>
2022-03-01 00:18:46 +0600

    first step complete all phones show in display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log('is it ok for you');
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+
+    // connect with link
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
+}
+
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    // console.log(phones.brand);
+
+    phones.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
